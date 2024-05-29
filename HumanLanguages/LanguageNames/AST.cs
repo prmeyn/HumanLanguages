dd45c3c8b299c41dd8ebc1791539b174be41d74f
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-05-29 15:14:48 +0200

    Update AST.cs
    asturian variations done. 29-5-2024 kl 15 14

--- HumanLanguages/LanguageNames/AST.cs
+++ HumanLanguages/LanguageNames/AST.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.ES, "Spain" },
+				{ LanguageLocaleVariationCode.ES, "España (Spain)" },
 			});
 	}
 }
