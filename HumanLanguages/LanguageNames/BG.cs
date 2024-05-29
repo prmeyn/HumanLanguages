4e46a2e3b4f1558232809ee1888cccfbb83e8c6f
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-05-29 15:32:39 +0200

    Update BG.cs
    bulgarian variations done 29-5-2024 kl 15 32

--- HumanLanguages/LanguageNames/BG.cs
+++ HumanLanguages/LanguageNames/BG.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.BG, "Bulgaria" },
+				{ LanguageLocaleVariationCode.BG, "България (Bulgaria)" },
 			});
 	}
 }
