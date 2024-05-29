6c95757beae6c3d79304e952ad0118ab3574923f
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-05-29 15:26:34 +0200

    Update BE.cs
    belarussian variations done 29-5-2024 kl 15 26

--- HumanLanguages/LanguageNames/BE.cs
+++ HumanLanguages/LanguageNames/BE.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.BY, "Belarus" },
+				{ LanguageLocaleVariationCode.BY, "Беларусь (Belarus)" },
 			});
 	}
 }
