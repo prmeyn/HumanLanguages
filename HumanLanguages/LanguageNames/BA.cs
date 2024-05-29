512891e8001b1fb2ff7b49f11fbabf617aee809e
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-05-29 15:23:07 +0200

    Update BA.cs
    bashkir variations done 29-5-2024 kl 15 23

--- HumanLanguages/LanguageNames/BA.cs
+++ HumanLanguages/LanguageNames/BA.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.RU, "Russia" },
+				{ LanguageLocaleVariationCode.RU, "Рәсәй (Russia)" },
 			});
 	}
 }
