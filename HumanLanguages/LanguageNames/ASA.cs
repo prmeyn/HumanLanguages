af73fa5891637e0b537af56460825697e68ff18c
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-05-29 15:13:41 +0200

    Update ASA.cs
    asu variations done 29-5-2024 kl 15 13

--- HumanLanguages/LanguageNames/ASA.cs
+++ HumanLanguages/LanguageNames/ASA.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.TZ, "Tanzania" },
+				{ LanguageLocaleVariationCode.TZ, "Wutanzania (Tanzania)" },
 			});
 	}
 }
