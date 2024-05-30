2b79599465e15ed37cec18777bd7805f9f676b14
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-05-30 16:03:23 +0200

    Update DZ.cs
    Dzongkha variations done 30-5-2024 kl 16 03

--- HumanLanguages/LanguageNames/DZ.cs
+++ HumanLanguages/LanguageNames/DZ.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.BT, "Bhutan" },
+				{ LanguageLocaleVariationCode.BT, "འབྲུག་ཡུལ (Bhutan)" },
 			});
 	}
 }
