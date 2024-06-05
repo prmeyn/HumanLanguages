e7506efa398d25a6435313606979acc3ee15c739
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 17:57:57 +0200

    Update TH.cs
    thai done

--- HumanLanguages/LanguageNames/TH.cs
+++ HumanLanguages/LanguageNames/TH.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.TH, "Thailand" },
+				{ LanguageLocaleVariationCode.TH, "ประเทศไทย (Thailand)" },
 			});
 	}
 }
