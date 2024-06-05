16827e0a954fb8542678d38ca7e9eed46232584b
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 14:40:26 +0200

    Update LV.cs
    latvian done

--- HumanLanguages/LanguageNames/LV.cs
+++ HumanLanguages/LanguageNames/LV.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.LV, "Latvia" },
+				{ LanguageLocaleVariationCode.LV, "Latvija (Latvia)" },
 			});
 	}
 }
