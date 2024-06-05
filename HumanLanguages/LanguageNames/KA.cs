8959828ff073b99ade92b419fb3e15de5ad5bcaa
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:22:42 +0200

    Update KA.cs
    Georgian variations done 5-6-2024

--- HumanLanguages/LanguageNames/KA.cs
+++ HumanLanguages/LanguageNames/KA.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.GE, "Georgia" },
+				{ LanguageLocaleVariationCode.GE, "საქართველო (Georgia)" },
 			});
 	}
 }
