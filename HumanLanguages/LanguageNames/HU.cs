8562fee28e1f1cf150ff3dfcdbc8c6fe97da4a1e
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:00:35 +0200

    Update HU.cs
    Hungarian variations done 5-6-2024 kl 13 00

--- HumanLanguages/LanguageNames/HU.cs
+++ HumanLanguages/LanguageNames/HU.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.HU, "Hungary" },
+				{ LanguageLocaleVariationCode.HU, "Magyarország (Hungary)" },
 			});
 	}
 }
