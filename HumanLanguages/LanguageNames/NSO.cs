521435959c4be4bb0bbf27561499438305560f52
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:45:34 +0200

    Update NSO.cs
    Sesotho sa Leboa done

--- HumanLanguages/LanguageNames/NSO.cs
+++ HumanLanguages/LanguageNames/NSO.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.ZA, "South Africa" },
+				{ LanguageLocaleVariationCode.ZA, "Afrika Borwa (South Africa)" },
 			});
 	}
 }
