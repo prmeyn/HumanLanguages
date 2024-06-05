3ce4542c767a47026e8ec35e7e476b9c52038960
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:23:48 +0200

    Update NAQ.cs
    nama done

--- HumanLanguages/LanguageNames/NAQ.cs
+++ HumanLanguages/LanguageNames/NAQ.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.NA, "Namibia" },
+				{ LanguageLocaleVariationCode.NA, "Namibiab (Namibia)" },
 			});
 	}
 }
