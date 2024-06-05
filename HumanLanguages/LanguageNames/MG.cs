384ec106ac9e2941e39bbc753c4a41e175705bec
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:01:43 +0200

    Update MG.cs
    malagasy done

--- HumanLanguages/LanguageNames/MG.cs
+++ HumanLanguages/LanguageNames/MG.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.MG, "Madagascar" },
+				{ LanguageLocaleVariationCode.MG, "Madagasikara (Madagascar)" },
 			});
 	}
 }
