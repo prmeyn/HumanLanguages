77ce17bb899cfa8b27f598e6be180b962b54e290
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:12:52 +0200

    Update MOH.cs
    mohawk done

--- HumanLanguages/LanguageNames/MOH.cs
+++ HumanLanguages/LanguageNames/MOH.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.CA, "Canada" },
+				{ LanguageLocaleVariationCode.CA, "Kanata (Canada)" },
 			});
 	}
 }
