0f24a093ffa74c5cd4353cfe25db6d09b201239e
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 12:33:09 +0200

    Update FUR.cs
    Friulian variations done 5-6-2024 kl 12 33

--- HumanLanguages/LanguageNames/FUR.cs
+++ HumanLanguages/LanguageNames/FUR.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.IT, "Italy" },
+				{ LanguageLocaleVariationCode.IT, "Italie (Italy)" },
 			});
 	}
 }
