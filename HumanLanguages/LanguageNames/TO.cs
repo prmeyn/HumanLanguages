89275d5971222198e8875f9d1c9b77f50e15d710
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 18:06:04 +0200

    Update TO.cs
    tonga done

--- HumanLanguages/LanguageNames/TO.cs
+++ HumanLanguages/LanguageNames/TO.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.TO, "Tonga" },
+				{ LanguageLocaleVariationCode.TO, "Tonga" },//same name
 			});
 	}
 }
