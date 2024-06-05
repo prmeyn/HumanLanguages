e3b3fb859ee69647ffa84314c7a60e07bac0dbca
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:59:04 +0200

    Update PRG.cs
    Prussian done

--- HumanLanguages/LanguageNames/PRG.cs
+++ HumanLanguages/LanguageNames/PRG.cs
@@ -248,7 +248,7 @@ namespace HumanLanguages.LanguageNames
 				{ LanguageId.zu, "IsiPrussia" },
 			},
 			VariationNativeNames:
-			new Dictionary<LanguageLocaleVariationCode, string>()
+			new Dictionary<LanguageLocaleVariationCode, string>()// no variations
 			{
 			});
 	}
