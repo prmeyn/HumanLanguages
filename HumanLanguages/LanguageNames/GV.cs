4182fcac249ce393dd761d6f262a5ef00b63c7b7
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 12:47:30 +0200

    Update GV.cs
    Manx variations done 5-6-2024 kl 12 47

--- HumanLanguages/LanguageNames/GV.cs
+++ HumanLanguages/LanguageNames/GV.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.IM, "Isle of Man" },
+				{ LanguageLocaleVariationCode.IM, "Ellan Vannin (Isle of Man)" },
 			});
 	}
 }
