e14d096be03241326d893fb7224b7c63553e7de8
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 12:35:44 +0200

    Update FY.cs
    Western Frisian variations done. 5-6-2024 kl 12 35

--- HumanLanguages/LanguageNames/FY.cs
+++ HumanLanguages/LanguageNames/FY.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.NL, "Netherlands" },
+				{ LanguageLocaleVariationCode.NL, "Nederlân (Netherlands)" },
 			});
 	}
 }
