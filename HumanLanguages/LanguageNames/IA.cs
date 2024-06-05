cf20b0fe21ddb8d6ac38d6bcd05a74a2f463741e
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:02:39 +0200

    Update IA.cs
    Interlingua - no variations 5-6-2024 kl 13 02

--- HumanLanguages/LanguageNames/IA.cs
+++ HumanLanguages/LanguageNames/IA.cs
@@ -248,7 +248,7 @@ namespace HumanLanguages.LanguageNames
 				{ LanguageId.zu, "Isi-Interlingua" },
 			},
 			VariationNativeNames:
-			new Dictionary<LanguageLocaleVariationCode, string>()
+			new Dictionary<LanguageLocaleVariationCode, string>()// no variations
 			{
 			});
 	}
