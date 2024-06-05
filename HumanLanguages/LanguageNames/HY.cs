8456677fcb2c85fe6e4392ca9a271102a841f46d
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:01:39 +0200

    Update HY.cs
    Armenian variations done 5-6- 2024 kl 13 01

--- HumanLanguages/LanguageNames/HY.cs
+++ HumanLanguages/LanguageNames/HY.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.AM, "Armenia" },
+				{ LanguageLocaleVariationCode.AM, "Հայաստան (Armenia)" },
 			});
 	}
 }
