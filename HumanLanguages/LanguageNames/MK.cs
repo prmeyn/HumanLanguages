dfd20cef2ea4e6ed93408aaf238e9b09cfd529d2
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:07:12 +0200

    Update MK.cs
    macedonian done

--- HumanLanguages/LanguageNames/MK.cs
+++ HumanLanguages/LanguageNames/MK.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.MK, "North Macedonia" },
+				{ LanguageLocaleVariationCode.MK, "Северна Македонија (North Macedonia)" },
 			});
 	}
 }
