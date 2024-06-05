f4031013a5450ed94dfd007fa68370f1c0244a7b
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:44:34 +0200

    Update KAB.cs
    Kabyle variations done 5-6-2024 kl 13 44

--- HumanLanguages/LanguageNames/KAB.cs
+++ HumanLanguages/LanguageNames/KAB.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.DZ, "Algeria" },
+				{ LanguageLocaleVariationCode.DZ, "Ldzayer (Algeria)" },
 			});
 	}
 }
