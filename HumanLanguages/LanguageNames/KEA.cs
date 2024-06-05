0b5b9e216dcb81fe3f7e0cef67f8e3da20e5de24
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:48:35 +0200

    Update KEA.cs
    Kabuverdianu done

--- HumanLanguages/LanguageNames/KEA.cs
+++ HumanLanguages/LanguageNames/KEA.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.CV, "Cabo Verde" },
+				{ LanguageLocaleVariationCode.CV, "Cabo Verde" },//same name
 			});
 	}
 }
