3d60e0c5c0e421244f16943b475c5c12027ae82a
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 18:29:28 +0200

    Update XOG.cs
    soga done

--- HumanLanguages/LanguageNames/XOG.cs
+++ HumanLanguages/LanguageNames/XOG.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.UG, "Uganda" },
+				{ LanguageLocaleVariationCode.UG, "Yuganda (Uganda)" },
 			});
 	}
 }
