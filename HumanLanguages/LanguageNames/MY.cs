d28f34cb7155facaf72adea18e5d3e74e424d827
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:18:58 +0200

    Update MY.cs
    burmese done

--- HumanLanguages/LanguageNames/MY.cs
+++ HumanLanguages/LanguageNames/MY.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.MM, "Myanmar" },
+				{ LanguageLocaleVariationCode.MM, "မြန်မာ (Myanmar)" },
 			});
 	}
 }
