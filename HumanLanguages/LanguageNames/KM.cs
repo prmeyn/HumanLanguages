e9eec117454cab10c31ee36be4fb5f4784be9953
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 14:08:48 +0200

    Update KM.cs
    khmer done

--- HumanLanguages/LanguageNames/KM.cs
+++ HumanLanguages/LanguageNames/KM.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.KH, "Cambodia" },
+				{ LanguageLocaleVariationCode.KH, "កម្ពុជា (Cambodia)" },
 			});
 	}
 }
