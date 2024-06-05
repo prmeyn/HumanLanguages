0e9529cfc10e17a0a52a21c1f98830527d3d233c
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:16:43 +0200

    Update MT.cs
    maltese done

--- HumanLanguages/LanguageNames/MT.cs
+++ HumanLanguages/LanguageNames/MT.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.MT, "Malta" },
+				{ LanguageLocaleVariationCode.MT, "Malta" },// same name
 			});
 	}
 }
