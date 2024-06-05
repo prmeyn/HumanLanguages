1d472abf5ccdba04f85a51639f2cf404a1863dfd
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:08:23 +0200

    Update IS.cs
    Icelandic variations done 5-6-2024 kl 13 08

--- HumanLanguages/LanguageNames/IS.cs
+++ HumanLanguages/LanguageNames/IS.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.IS, "Iceland" },
+				{ LanguageLocaleVariationCode.IS, "Ísland (Iceland)" },
 			});
 	}
 }
