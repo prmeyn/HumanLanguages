2d98e42bb2f141a16b07f0b449f180dcaf5b8c0a
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:21:54 +0200

    Update MZN.cs
    Mazanderani done

--- HumanLanguages/LanguageNames/MZN.cs
+++ HumanLanguages/LanguageNames/MZN.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.IR, "Iran" },
+				{ LanguageLocaleVariationCode.IR, "ایران (Iran)" },
 			});
 	}
 }
