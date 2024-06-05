ebbb64d4e53a4881b0a88ad2846afd7a11a2eed1
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 14:29:00 +0200

    Update LB.cs
    Luxembourgish done

--- HumanLanguages/LanguageNames/LB.cs
+++ HumanLanguages/LanguageNames/LB.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.LU, "Luxembourg" },
+				{ LanguageLocaleVariationCode.LU, "Lëtzebuerg (Luxembourg)" },
 			});
 	}
 }
