dcc5689839010b78078b7892deb9a48180f2118d
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 12:54:50 +0200

    Update HE.cs
    Hebrew variations done 5-6-2024 kl 12 54

--- HumanLanguages/LanguageNames/HE.cs
+++ HumanLanguages/LanguageNames/HE.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.IL, "Israel" },
+				{ LanguageLocaleVariationCode.IL, "ישראל (Israel)" },
 			});
 	}
 }
