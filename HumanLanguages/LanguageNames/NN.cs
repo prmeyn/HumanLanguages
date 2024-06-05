195c74034b064121633fbb204ae659ff1a3be728
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:38:24 +0200

    Update NN.cs
    Norwegian Nynorsk done

--- HumanLanguages/LanguageNames/NN.cs
+++ HumanLanguages/LanguageNames/NN.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.NO, "Norway" },
+				{ LanguageLocaleVariationCode.NO, "Noreg (Norway)" },
 			});
 	}
 }
