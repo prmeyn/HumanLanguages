aff419424d73ffac2ab2ee36d2ee3b3da5250e9f
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:57:28 +0200

    Update PCM.cs
    Nigerian Pidgin done

--- HumanLanguages/LanguageNames/PCM.cs
+++ HumanLanguages/LanguageNames/PCM.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.NG, "Nigeria" },
+				{ LanguageLocaleVariationCode.NG, "Naija (Nigeria)" },
 			});
 	}
 }
