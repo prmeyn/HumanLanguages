e345a513d6eeb28c6114e1e8c0c9b645196eb36b
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 12:50:13 +0200

    Update HAW.cs
    Hawaiian variations done 5-6-2024 kl 12 50

--- HumanLanguages/LanguageNames/HAW.cs
+++ HumanLanguages/LanguageNames/HAW.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.US, "United States" },
+				{ LanguageLocaleVariationCode.US, "Amelika Hui Pū ʻIa (United States)" },
 			});
 	}
 }
