5d6fb3de208958f120dea9de1944cdaf180962ca
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 17:01:21 +0200

    Update SMN.cs
    inari sami done

--- HumanLanguages/LanguageNames/SMN.cs
+++ HumanLanguages/LanguageNames/SMN.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.FI, "Finland" },
+				{ LanguageLocaleVariationCode.FI, "Suopma (Finland)" },
 			});
 	}
 }
