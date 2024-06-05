7b92eaf3b33b1b3bed309003e3409b5515fa287c
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 18:12:50 +0200

    Update UG.cs
    Uyghur done

--- HumanLanguages/LanguageNames/UG.cs
+++ HumanLanguages/LanguageNames/UG.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.CN, "China" },
+				{ LanguageLocaleVariationCode.CN, "جۇڭگو (China)" },
 			});
 	}
 }
