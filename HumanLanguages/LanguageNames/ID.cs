5d350e24f6ca578e8fcffb55f8bc7c73b12250d0
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:05:09 +0200

    Update ID.cs
    Indonesian variations done 5-6-2024 kl 13 05

--- HumanLanguages/LanguageNames/ID.cs
+++ HumanLanguages/LanguageNames/ID.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.ID, "Indonesia" },
+				{ LanguageLocaleVariationCode.ID, "Indonesia" },//name remains same
 			});
 	}
 }
