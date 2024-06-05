d61450455b3c8a56926b0c7bad0fd2d68bb94b06
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 18:27:07 +0200

    Update WO.cs
    wolof done

--- HumanLanguages/LanguageNames/WO.cs
+++ HumanLanguages/LanguageNames/WO.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.SN, "Senegal" },
+				{ LanguageLocaleVariationCode.SN, "Sunugaal (Senegal)" },
 			});
 	}
 }
