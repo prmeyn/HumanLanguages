0049e1e1ba056416f5570b4886148d68a57d23e5
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 15:58:28 +0200

    Update PL.cs
    polish done

--- HumanLanguages/LanguageNames/PL.cs
+++ HumanLanguages/LanguageNames/PL.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.PL, "Poland" },
+				{ LanguageLocaleVariationCode.PL, "Polska (Poland)" },
 			});
 	}
 }
