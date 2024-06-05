5d210e016fc006772f4e950a21b8e97361970520
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 14:37:59 +0200

    Update LT.cs
    Lithuanian done

--- HumanLanguages/LanguageNames/LT.cs
+++ HumanLanguages/LanguageNames/LT.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.LT, "Lithuania" },
+				{ LanguageLocaleVariationCode.LT, "Lietuva (Lithuania)" },
 			});
 	}
 }
