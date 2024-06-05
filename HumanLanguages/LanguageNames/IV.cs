aaefaefdbfa1ba6551cd6169860a4637ba557c5d
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 13:15:33 +0200

    Update IV.cs
    IV no variations 5-6-2024 kl 13 15

--- HumanLanguages/LanguageNames/IV.cs
+++ HumanLanguages/LanguageNames/IV.cs
@@ -248,7 +248,7 @@ namespace HumanLanguages.LanguageNames
 				{ LanguageId.zu, "" },
 			},
 			VariationNativeNames:
-			new Dictionary<LanguageLocaleVariationCode, string>()
+			new Dictionary<LanguageLocaleVariationCode, string>()// no variations
 			{
 			});
 	}
