4300cc2e5a3e4d5c3da84acf029a7fd2503f0e29
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 14:45:56 +0200

    Update MFE.cs
    morisyen done

--- HumanLanguages/LanguageNames/MFE.cs
+++ HumanLanguages/LanguageNames/MFE.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.MU, "Mauritius" },
+				{ LanguageLocaleVariationCode.MU, "Moris (Mauritius)" },
 			});
 	}
 }
