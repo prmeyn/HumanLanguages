ef094e7eed9785a1f60a0814dcf304df50355c4f
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 18:26:26 +0200

    Update WAL.cs
    Wolaytta done

--- HumanLanguages/LanguageNames/WAL.cs
+++ HumanLanguages/LanguageNames/WAL.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.ET, "Ethiopia" },
+				{ LanguageLocaleVariationCode.ET, "ኢትዮጵያ (Ethiopia)" },
 			});
 	}
 }
