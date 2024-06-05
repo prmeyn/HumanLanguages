37eb4e03f574bc89d65beb73b0fb11ba69121a0c
Prashansa Meyn <100074068+prmeyn@users.noreply.github.com>
2024-06-05 17:36:47 +0200

    Update SYR.cs
    syriac done

--- HumanLanguages/LanguageNames/SYR.cs
+++ HumanLanguages/LanguageNames/SYR.cs
@@ -250,7 +250,7 @@ namespace HumanLanguages.LanguageNames
 			VariationNativeNames:
 			new Dictionary<LanguageLocaleVariationCode, string>()
 			{
-				{ LanguageLocaleVariationCode.SY, "Syria" },
+				{ LanguageLocaleVariationCode.SY, "ܣܘܪܝܐ (Syria)" },
 			});
 	}
 }
